03c0f44a2e70d48db19fafba2073319a65d2b963
Ferno <154271878+Fernoxx@users.noreply.github.com>
2025-09-03 03:52:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
     <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
         </div>
-      </QueryClientProvider>
     </WagmiProvider>
   );
 }
