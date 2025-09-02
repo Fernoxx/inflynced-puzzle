21fe59f57ed08a029cecb2af9d8137302787d527
Ferno <154271878+Fernoxx@users.noreply.github.com>
2025-09-03 03:52:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import './App.css';
+import InflyncedPuzzle from './InflyncedPuzzle';
+import { WagmiProvider } from 'wagmi';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { wagmiConfig } from './wagmi';
 
+const queryClient = new QueryClient();
+
+function App() {
   return (
-        </div
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <div className="App">
+          <InflyncedPuzzle />
+        </div>
+      </QueryClientProvider>
+    </WagmiProvider>
   );
 }
 
