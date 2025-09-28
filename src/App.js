fea3019739e5339ed2594ca05d22e96b77dd4b1b
Ferno <154271878+Fernoxx@users.noreply.github.com>
2025-09-29 04:48:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import './App.css';
 import InflyncedPuzzle from './InflyncedPuzzle';
 import { WagmiProvider } from 'wagmi';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
 function App() {
   return (
         <div className="App">
