37f50e94464b43383a9b7f3762fb9890b6138c66
Ferno <154271878+Fernoxx@users.noreply.github.com>
2025-09-29 04:48:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import { WagmiProvider } from 'wagmi';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { wagmiConfig } from './wagmi';
 
-const queryClient = new QueryClient();
-
 function App() {
   return (
         <div className="App">
