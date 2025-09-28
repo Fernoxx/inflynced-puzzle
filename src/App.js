463bf4efaecb6f9f93f72cbbbeae3a27f490f64f
Ferno <154271878+Fernoxx@users.noreply.github.com>
2025-09-29 04:49:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import InflyncedPuzzle from './InflyncedPuzzle';
-import { WagmiProvider } from 'wagmi';
 function App() {
   return (
         <div className="App">
